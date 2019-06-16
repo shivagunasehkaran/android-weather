7b4a9b1dd05263868febd7425b193a5ed2ef0c77
shivagunasehkaran <danielshiva@gmail.com>
2019-06-17 02:49:22 +0800

    Created basic setup and dashboard layout for display cities weather

---
+++ app/src/main/java/com/example/shivagunasehkaran/weather/view/activity/DashboardActivity.java
@@ -0,0 +1,17 @@
+package com.example.shivagunasehkaran.weather.view.activity;
+
+import android.os.Bundle;
+
+import com.example.shivagunasehkaran.weather.R;
+
+public class DashboardActivity extends BaseActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override protected int getLayoutId() {
+        return R.layout.activity_dashboard;
+    }
+
+}
