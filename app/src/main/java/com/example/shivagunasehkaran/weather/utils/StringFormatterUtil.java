a240fce6f1a9d19de098e6882a225838b08ce168
shivagunasehkaran <danielshiva@gmail.com>
2019-06-17 23:06:23 +0800

    Updated 'StringbuilderUtils' with various methods to display weather details

--- app/src/main/java/com/example/shivagunasehkaran/weather/utils/StringFormatterUtil.java
+++ app/src/main/java/com/example/shivagunasehkaran/weather/utils/StringFormatterUtil.java
@@ -1,6 +1,11 @@
 package com.example.shivagunasehkaran.weather.utils;
 
+import android.text.format.DateFormat;
+
 import com.example.shivagunasehkaran.weather.model.WeatherPOJO;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public final class StringFormatterUtil {
 
@@ -8,8 +13,15 @@ public final class StringFormatterUtil {
         throw new AssertionError();
     }
 
-    public static String getTemperature(WeatherPOJO weatherResponse) {
+    public static String getPlace(WeatherPOJO weatherResponse) {
+        StringBuilder stringBuilder = new StringBuilder(weatherResponse.getName());
+        if (weatherResponse.getSys() != null) {
+            stringBuilder.append(", ").append(weatherResponse.getSys().getCountry());
+        }
+        return stringBuilder.toString();
+    }
 
+    public static String getTemperature(WeatherPOJO weatherResponse) {
         if (weatherResponse.getMain() != null &&
                 weatherResponse.getMain().getTemp() != null) {
             return getTemperatureString(weatherResponse.getMain().getTemp());
@@ -19,6 +31,26 @@ public final class StringFormatterUtil {
 
     private static String getTemperatureString(double temperature) {
         return String.valueOf(Math.round(temperature)).concat(" °C");
+    }
+
+    public static String getWind(WeatherPOJO weatherResponse) {
+        if (weatherResponse.getWind() != null) {
+            return String.valueOf(weatherResponse.getWind().getWindSpeed()).concat(" m/s");
+        }
+        return null;
+    }
+
+    public static String getDate(WeatherPOJO weatherResponse) {
+        if (weatherResponse.getDate() != null) {
+            return getConvertedDate(Long.parseLong(weatherResponse.getDate()));
+        }
+        return null;
+    }
+
+    private static String getConvertedDate(long timestamp) {
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(timestamp * 1000L);
+        return DateFormat.format("dd mmm yyyy", cal).toString();
     }
 
     public static String getDescription(WeatherPOJO weatherResponse) {
